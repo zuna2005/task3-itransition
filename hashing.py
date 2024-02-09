39b8e692c1436ca894cde173d58cf1aeeae127d1
zuna2005 <nasiba.zulunova@gmail.com>
2024-02-10 01:42:59 +0500

    without literals

--- hashing.py
+++ hashing.py
@@ -2,17 +2,16 @@ import secrets
 import hmac
 import hashlib
 
+NUM_BYTES = 64
+
 
 class Hmac:
     def __init__(self, moves):
-        self.key = secrets.token_hex(64)
+        self.key = secrets.token_hex(NUM_BYTES)
         self.move = secrets.choice(moves)
         self.move_index = moves.index(self.move)
 
     def get_hmac(self):
         hash_algorithm = hashlib.sha3_256
-
-        # Calculate the HMAC using the specified key and message
         hmac_digest = hmac.new(self.key.encode(), self.move.encode(), hash_algorithm).hexdigest()
-
         return hmac_digest
